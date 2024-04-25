fe692066e378cb3ae693865b43708a375b24c243
Li Wenjie <825170940@qq.com>
2024-04-25 17:13:37 +0800

    a

--- 1.js
+++ 1.js
@@ -1,1 +1,16 @@
-aa
+// ==UserScript==
+// @name         amazon-competitor
+// @namespace    qianyuanlinkun
+// @version      2024-04-25
+// @description  导出亚马逊竞品
+// @author       qianyuanlinkun
+// @match        https://*.amazon.*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("hello world")
+})();
