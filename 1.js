e2399d58c811fd1b227e54ae1ad1e9f8f9b6524a
Li Wenjie <825170940@qq.com>
2024-04-25 17:34:04 +0800

    a

--- 1.js
+++ 1.js
@@ -7,10 +7,11 @@
 // @match        https://*.amazon.*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @license      gpl-3.0
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    console.log("hello world")
+    console.log("hello world!")
 })();
